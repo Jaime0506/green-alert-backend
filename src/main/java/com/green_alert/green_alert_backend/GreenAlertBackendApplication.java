5b755dc398254064b8579eab9301c4a5208d3a94
Jaime M <jaimemejia1003@gmail.com>
2025-03-05 16:39:53 -0500

    first commit

---
+++ src/main/java/com/green_alert/green_alert_backend/GreenAlertBackendApplication.java
@@ -0,0 +1,12 @@
+package com.green_alert.green_alert_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenAlertBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenAlertBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/green_alert/green_alert_backend/GreenAlertBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green_alert.green_alert_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenAlertBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
